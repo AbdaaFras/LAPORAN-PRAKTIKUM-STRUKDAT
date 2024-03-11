4d5b9f28db47d6e12ff67eb4b0e8bb0e2a4a3f7a
AbdaaFras <162860926+AbdaaFras@users.noreply.github.com>
2024-03-11 14:14:55 +0700

    Revisi unguided1.cpp

--- Modul 1 Tipe Data/unguided1.cpp
+++ Modul 1 Tipe Data/unguided1.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
-class Kubus
-{
-    public:
-    double panjang;
-    double luas;
-    double tinggi;
-};
-int main()
-{
-    Kubus kubus1;
-    Kubus kubus2;
-    double volume = 0.0;
-    //data kubus 1
-    kubus1.tinggi = 5.0;
-    kubus1.panjang = 7.0;
-    kubus1.luas = 2.0;
-    //data kubus 2
-    kubus2.tinggi = 10.0;
-    kubus2.panjang = 14.0;
-    kubus2.luas = 12.0;
-    //volume dari kubus 1
-    volume = kubus1.tinggi * kubus1.panjang * kubus1.luas;
-    cout << "volume dari kubus 1 :" << volume << endl;
-    //volume dari kubus2
-    volume = kubus2.tinggi * kubus2.panjang * kubus2.luas;
-    cout << "volume dari kubus 2 :" << volume << endl;
-    getch();
+
+int hitungLuas(int panjang, int lebar){
+    return panjang * lebar;
+}
+
+int hitungVolume(int sisi){
+    return sisi * sisi * sisi;
+}
+
+int main(){
+    int panjang = 5;
+    int lebar = 3;
+    int sisi = 2;
+    int luas = hitungLuas(panjang, lebar);
+    int volume = hitungVolume(sisi);
+
+    cout << "Luas persegi panjang:" << luas << endl;
+    cout << "Volume kubus:" << volume << endl;
+    return 0;
 }
 
